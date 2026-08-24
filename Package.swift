97f4aad70cd259054157a368c91a91a3580dc685
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-24 12:40:26 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 let package = Package(
   name: "institute-application",
   platforms: [
-    .macOS("27"),
-    .iOS("27"),
-    .tvOS("27"),
-    .watchOS("27"),
-    .visionOS("27"),
+    .macOS(.v27),
+    .iOS(.v27),
+    .tvOS(.v27),
+    .watchOS(.v27),
+    .visionOS(.v27),
   ],
   products: [
     .library(
